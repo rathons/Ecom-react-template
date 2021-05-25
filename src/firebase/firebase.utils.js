f7ede6cbd6aa45ce97a5fb354d4214576850f2a1
Rathon Sriram <rathon.s@gmail.com>
2021-05-25 22:24:10 +0530

    Added Firebase and SignIn Functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDDrlTZ_E04lUJFsk2zIijeTc_8rgyI404",
-    authDomain: "ecom-react-template.firebaseapp.com",
-    projectId: "ecom-react-template",
-    storageBucket: "ecom-react-template.appspot.com",
-    messagingSenderId: "282691855968",
-    appId: "1:282691855968:web:9df48262a28de1e69bc505",
-    measurementId: "G-D569DMQPVH"
+    
   };
 
   firebase.initializeApp(config);
